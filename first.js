7fb6a45f0bf4f60ef27c7660f42b61db6a2bc803
NAJEEBKHAN123 <145932116+NAJEEBKHAN123@users.noreply.github.com>
2024-01-08 21:16:51 +0500

    add

--- first.js
+++ first.js
@@ -3,3 +3,11 @@ const header = document.querySelector("header");
 window.classList.toggle("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 120);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
